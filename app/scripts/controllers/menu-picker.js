1ef6bac9c0e3eabad82090d2d83ec62fc865bdf3
Stuart Pearlman <stupearlman@gmail.com>
2016-03-20 22:48:17 -0500

    Generate string of menu items on menu model

--- app/scripts/controllers/menu-picker.js
+++ app/scripts/controllers/menu-picker.js
@@ -3,5 +3,26 @@
 angular.module('everyPenny')
 
 .controller('MenuPicker', function(Menu) {
-  this.menu = new Menu({ budget: 12 });
+  this.menu = new Menu({
+    budget: 15.05,
+    menuItems: [{
+      name: 'mixed fruit',
+      price: 2.15
+    }, {
+      name: 'french fries',
+      price: 2.75
+    }, {
+      name: 'side salad',
+      price: 3.35
+    }, {
+      name: 'hot wings',
+      price: 3.55
+    }, {
+      name: 'mozzarella sticks',
+      price: 4.20
+    }, {
+      name: 'sampler plate',
+      price: 5.80
+    }]
+  });
 });
--- app/scripts/services/menu.js
+++ app/scripts/services/menu.js
@@ -3,9 +3,46 @@
 angular.module('everyPenny')
 
 .factory('Menu', function() {
+  var toHundrethsPlace = function(number) {
+    return Math.round(number * 100) / 100;
+  };
+
   var Menu = function(configOptions) {
     this.budget = configOptions.budget || 0;
     this.menuItems = configOptions.menuItems || [];
+    this.menuString = configOptions.menuString || '';
+
+    this.clearMenuString = function() {
+      this.menuString = '';
+    };
+
+    this.generateMenu = function(total, returnedMenuItemsString, menu) {
+      total = toHundrethsPlace(total || this.budget);
+      returnedMenuItemsString = returnedMenuItemsString || '';
+      menu = menu || this.menuItems;
+
+      var cachedMenu = JSON.parse(JSON.stringify(menu));
+
+      menu.forEach(function(item) {
+        var price = toHundrethsPlace(item.price);
+        if (total - price === 0) {
+          if (this.menuString) {
+            this.menuString = this.menuString + ' AND';
+          }
+          this.menuString = this.menuString + returnedMenuItemsString + ' ' + item.name;
+          return;
+        }
+        if (total - price > 0) {
+          this.generateMenu(total - price, returnedMenuItemsString + ' ' + item.name, cachedMenu);
+        }
+        for (var i = 0; i < cachedMenu.length; i++) {
+          if (cachedMenu[i].name === item.name) {
+            cachedMenu.splice(i, 1);
+            break;
+          }
+        }
+      }.bind(this));
+    };
   };
 
   return Menu;
