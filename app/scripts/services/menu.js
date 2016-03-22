75328b337647f35cd10ebc5edc9c6d1663a0973a
Stuart Pearlman <stupearlman@gmail.com>
2016-03-21 22:19:15 -0500

    Begin updating menu model to use ES6

--- app/scripts/services/menu.js
+++ app/scripts/services/menu.js
@@ -2,12 +2,8 @@
 
 angular.module('everyPenny')
 
-.factory('Menu', function() {
-  var toHundrethsPlace = function(number) {
-    return Math.round(number * 100) / 100;
-  };
-
-  var Menu = function(configOptions) {
+.factory('Menu', function(_) {
+  let Menu = function(configOptions) {
     this.budget = configOptions.budget || 0;
     this.menuItems = configOptions.menuItems || [];
     this.menuString = configOptions.menuString || '';
@@ -17,31 +13,30 @@ angular.module('everyPenny')
     };
 
     this.generateMenu = function(total, returnedMenuItemsString, menu) {
-      total = toHundrethsPlace(total || this.budget);
+      total = _.round(total || this.budget, 2);
       returnedMenuItemsString = returnedMenuItemsString || '';
       menu = menu || this.menuItems;
 
-      var cachedMenu = JSON.parse(JSON.stringify(menu));
+      let cachedMenu = _.clone(menu);
 
-      menu.forEach(function(item) {
-        var price = toHundrethsPlace(item.price);
+      menu.forEach((item) => {
+        let price = _.round(item.price, 2);
+
         if (total - price === 0) {
           if (this.menuString) {
             this.menuString = this.menuString + ' AND';
           }
+
           this.menuString = this.menuString + returnedMenuItemsString + ' ' + item.name;
           return;
         }
+
         if (total - price > 0) {
           this.generateMenu(total - price, returnedMenuItemsString + ' ' + item.name, cachedMenu);
         }
-        for (var i = 0; i < cachedMenu.length; i++) {
-          if (cachedMenu[i].name === item.name) {
-            cachedMenu.splice(i, 1);
-            break;
-          }
-        }
-      }.bind(this));
+
+        _.remove(cachedMenu, (cachedMenuItem) => cachedMenuItem.name === item.name);
+      });
     };
   };
 
